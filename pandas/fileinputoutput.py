85bfae6ff525fafe36d4cd75c4f2caca2b6baf4e
sarant1 <samuel.arant10@gmail.com>
2023-04-30 22:09:34 -0400

    added commit to sql

--- pandas/fileinputoutput.py
+++ pandas/fileinputoutput.py
@@ -6,22 +6,12 @@ cd_df = pd.read_csv('csv/ComputerSales.csv')
 #print(cd_df)
 
 cd_df2 = pd.read_excel('csv/Financial Sample.xlsx', 0)
-print(cd_df2)
+#print(cd_df2)
 
 # Convert csv to excel
 cd_df.to_excel('csv/ComputerSalesBUI.xlsx')
 
-def create_table_and_insert_data(cursor):
-    SQL_QUERY = """
-    CREATE TABLE IF NOT EXISTS student (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        age INT NOT NULL,
-        name VARCHAR(100) NOT NULL,
-        grade float NOT NULL,
-    )
-    """
-
-    cursor.execute(SQL_QUERY)
+def insertData(cursor):
 
     sample_data = [
         {'name': 'John Doe', 'age': 18, 'grade': 85.5},
@@ -33,30 +23,35 @@ def create_table_and_insert_data(cursor):
         cursor.execute(
             """
             INSERT INTO student 
-            (name, age, grade) 
+            (
+            name, 
+            age, 
+            grade
+            ) 
             VALUES (
                 %(name)s, 
                 %(age)s, 
-                %(grade)s)
-            """, 
-            data)
+                %(grade)s
+                )
+            """, data)
+    print("Data inserted successfully...")
 
 def get_db_connection():
-    return pymysql.connect(host='localhost',
+    return pymysql.connect(host='0.0.0.0',
                            user='root',
                            password='password',
-                           port=3306 
+                           database='testing',
+                           port=3306,
+                           cursorclass=pymysql.cursors.DictCursor 
                            )
 try: 
     connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("DROP TABLE IF EXISTS student")
-    create_table_and_insert_data(cursor)
-    stud_df = pd.read_sql('SELECT * FROM student', con=cursor)
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SHOW DATABASES;")
+            insertData(cursor)
+            connection.commit()
+    #stud_df = pd.read_sql('SELECT * FROM student', con=cursor)
 except Exception as e:
-    print("Error connecting to database\n")
+    print("\nError connecting to database")
     print(e)
-finally:
-    if connection:
-        connection.close()
-
